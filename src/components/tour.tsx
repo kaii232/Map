825a81fd7bffd3f87d3bddc6436134b2f9032a0a
Suu-ly <142672914+Suu-ly@users.noreply.github.com>
2025-05-01 16:59:53 +0800

    Removed completed and added memo for tour

--- src/components/tour.tsx
+++ src/components/tour.tsx
@@ -14,6 +14,7 @@ import { PopoverAnchor } from "@radix-ui/react-popover";
 import { atom, useAtom, useAtomValue, useSetAtom } from "jotai";
 import {
   createContext,
+  memo,
   ReactNode,
   useCallback,
   useContext,
@@ -30,7 +31,6 @@ const tourHighlightDimsAtom = atom<{
   left: number;
   top: number;
 }>();
-const tourCompletedAtom = atom(false);
 
 export type Steps = {
   /** `dialog` displays the step in a dialog, while `tooltip` displays the tour step over a highlighted component */
@@ -52,117 +52,115 @@ const TourContext = createContext<{
   onComplete?: () => void;
 }>({ steps: [] });
 
-const TourRoot = ({
-  steps,
-  tourId,
-  children,
-  shouldRememberCompletion = true,
-}: {
-  steps: Steps;
-  tourId: string;
-  children: ReactNode;
-  shouldRememberCompletion?: boolean;
-}) => {
-  // This uses 1 based indexing
-  // When step is 0 tour is not active
-  const [step, setStep] = useAtom(stepAtom);
-  const [completed, setCompleted] = useAtom(tourCompletedAtom);
-  const dimensions = useAtomValue(tourHighlightDimsAtom);
+const TourRoot = memo(
+  ({
+    steps,
+    tourId,
+    children,
+    shouldRememberCompletion = true,
+  }: {
+    steps: Steps;
+    tourId: string;
+    children: ReactNode;
+    shouldRememberCompletion?: boolean;
+  }) => {
+    // This uses 1 based indexing
+    // When step is 0 tour is not active
+    const [step, setStep] = useAtom(stepAtom);
+    const dimensions = useAtomValue(tourHighlightDimsAtom);
 
-  const onComplete = useCallback(() => {
-    setCompleted(true);
-    setStep(0);
-    if (shouldRememberCompletion)
-      localStorage.setItem(`tour-complete-${tourId}`, "true");
-  }, [setCompleted, setStep, shouldRememberCompletion, tourId]);
+    const onComplete = useCallback(() => {
+      setStep(0);
+      if (shouldRememberCompletion)
+        localStorage.setItem(`tour-complete-${tourId}`, "true");
+    }, [setStep, shouldRememberCompletion, tourId]);
 
-  // Start tour automatically if the user has not completed the tour yet
-  useEffect(() => {
-    if (!shouldRememberCompletion) {
-      setStep(1);
-      setCompleted(false);
-      return;
-    }
-    const pastCompletion = localStorage.getItem(`tour-complete-${tourId}`);
-    setCompleted(!!pastCompletion);
-    if (!pastCompletion) {
-      setStep(1);
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+    // Start tour automatically if the user has not completed the tour yet
+    useEffect(() => {
+      if (!shouldRememberCompletion) {
+        setStep(1);
+        return;
+      }
+      const pastCompletion = localStorage.getItem(`tour-complete-${tourId}`);
+      if (!pastCompletion) {
+        setStep(1);
+      }
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
 
-  const value = useMemo(
-    () => ({
-      steps,
-      onComplete,
-    }),
-    [onComplete, steps],
-  );
+    const value = useMemo(
+      () => ({
+        steps,
+        onComplete,
+      }),
+      [onComplete, steps],
+    );
 
-  if (completed) return children;
-
-  return (
-    <TourContext.Provider value={value}>
-      {children}
-      {steps[step - 1] && steps[step - 1].type === "dialog" && (
-        <Dialog open={steps[step - 1].type === "dialog"} modal>
-          <DialogContent
-            overlay={false}
-            closeButton={false}
-            className="data-[state=closed]:duration-0"
-          >
-            <DialogHeader>
-              <span className="text-sm text-neutral-400">
-                {step} of {steps.length}
-              </span>
-              <DialogTitle>{steps[step - 1].title}</DialogTitle>
-              <DialogDescription>
-                {steps[step - 1].description}
-              </DialogDescription>
-            </DialogHeader>
-            <DialogFooter>
-              {step > 1 && (
+    return (
+      <TourContext.Provider value={value}>
+        {children}
+        {steps[step - 1] && steps[step - 1].type === "dialog" && (
+          <Dialog open={steps[step - 1].type === "dialog"} modal>
+            <DialogContent
+              overlay={false}
+              closeButton={false}
+              className="data-[state=closed]:duration-0"
+            >
+              <DialogHeader>
+                <span className="text-sm text-neutral-400">
+                  {step} of {steps.length}
+                </span>
+                <DialogTitle>{steps[step - 1].title}</DialogTitle>
+                <DialogDescription>
+                  {steps[step - 1].description}
+                </DialogDescription>
+              </DialogHeader>
+              <DialogFooter>
+                {step > 1 && (
+                  <Button
+                    variant="ghost"
+                    onClick={() => {
+                      if (steps[step - 1].goBackStep)
+                        steps[step - 1].goBackStep!();
+                      setStep((prev) => prev - 1);
+                    }}
+                  >
+                    Back
+                  </Button>
+                )}
                 <Button
-                  variant="ghost"
                   onClick={() => {
-                    if (steps[step - 1].goBackStep)
-                      steps[step - 1].goBackStep!();
-                    setStep((prev) => prev - 1);
+                    if (steps[step - 1].afterStep) steps[step - 1].afterStep!();
+                    if (step < steps.length) setStep((prev) => prev + 1);
+                    else if (onComplete) onComplete();
                   }}
                 >
-                  Back
+                  {step === steps.length ? "End Tour" : "Next"}
                 </Button>
-              )}
-              <Button
-                onClick={() => {
-                  if (steps[step - 1].afterStep) steps[step - 1].afterStep!();
-                  if (step < steps.length) setStep((prev) => prev + 1);
-                  else if (onComplete) onComplete();
-                }}
-              >
-                {step === steps.length ? "End Tour" : "Next"}
-              </Button>
-            </DialogFooter>
-          </DialogContent>
-        </Dialog>
-      )}
-      {steps[step - 1] &&
-        createPortal(
-          <div
-            className="fixed inset-0 z-50 bg-neutral-800/80"
-            style={
-              dimensions && steps[step - 1].type === "tooltip"
-                ? {
-                    clipPath: `path("M ${window.innerWidth} ${window.innerHeight} H 0 V 0 H ${window.innerWidth} V ${window.innerHeight} Z M ${dimensions.left} ${dimensions.top - 8} a 8 8 0 0 0 -8 8 v ${dimensions.height} a 8 8 0 0 0 8 8 h ${dimensions.width} a 8 8 0 0 0 8 -8 v ${-dimensions.height} a 8 8 0 0 0 -8 -8 Z")`,
-                  }
-                : undefined
-            }
-          ></div>,
-          document.body,
+              </DialogFooter>
+            </DialogContent>
+          </Dialog>
         )}
-    </TourContext.Provider>
-  );
-};
+        {steps[step - 1] &&
+          createPortal(
+            <div
+              className="fixed inset-0 z-50 bg-neutral-800/80"
+              style={
+                dimensions && steps[step - 1].type === "tooltip"
+                  ? {
+                      clipPath: `path("M ${window.innerWidth} ${window.innerHeight} H 0 V 0 H ${window.innerWidth} V ${window.innerHeight} Z M ${dimensions.left} ${dimensions.top - 8} a 8 8 0 0 0 -8 8 v ${dimensions.height} a 8 8 0 0 0 8 8 h ${dimensions.width} a 8 8 0 0 0 8 -8 v ${-dimensions.height} a 8 8 0 0 0 -8 -8 Z")`,
+                    }
+                  : undefined
+              }
+            ></div>,
+            document.body,
+          )}
+      </TourContext.Provider>
+    );
+  },
+);
+
+TourRoot.displayName = "TourRoot";
 
 /** Displays a tooltip around a child element */
 const TourStep = ({
@@ -185,7 +183,6 @@ const TourStep = ({
 }) => {
   const [currentStep, setStep] = useAtom(stepAtom);
   const { steps, onComplete } = useContext(TourContext);
-  const completed = useAtomValue(tourCompletedAtom);
   const setDimensions = useSetAtom(tourHighlightDimsAtom);
   const containerRef = useRef<HTMLDivElement>(null);
   const ticking = useRef<boolean>(false);
@@ -221,8 +218,6 @@ const TourStep = ({
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [currentStep]);
-
-  if (completed) return children;
 
   if (!steps[step - 1]) throw new Error("Step is not defined!");
 
@@ -283,13 +278,11 @@ const TourStep = ({
 
 const TourStart = (props: ButtonProps) => {
   const setStep = useSetAtom(stepAtom);
-  const setCompleted = useSetAtom(tourCompletedAtom);
   return (
     <Button
       {...props}
       onClick={() => {
         setStep(1);
-        setCompleted(false);
       }}
     >
       Start Tour
